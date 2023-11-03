385c0a54a46825767dbd1a47f842af9401ee7ff6
RobertaKod <roberta00110110@icloud.com>
2023-11-03 16:55:20 +0100

    file read write

---
+++ file.py
@@ -0,0 +1,16 @@
+import os
+file = open("/Users/roberta/Documents/IOSDEV/PythonLab/PythonTest.testo.txt","r")
+print(file.read())
+print(file.readlines())
+#file.write("Sunday")
+##file.write("Monday")
+#file = open("/Users/roberta/Documents/IOSDEV/PythonLab/PythonTest.writetest.txt","x")
+if os.path.exists("/Users/roberta/Documents/IOSDEV/PythonLab/PythonTest/writetest.txt"):
+        os.remove("writetest.txt")
+else:
+    print(" There is no file")
+
+for line in file:
+      print(file.readlines())
+
+file.close()
